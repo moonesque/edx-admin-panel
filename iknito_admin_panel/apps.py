1a7454a4524184331fe72d3bf94207b7251aa417
Mahan <mahan.abbuspour@gmail.com>
2021-07-25 19:00:08 +0430

    Django app plugin

--- iknito_admin_panel/apps.py
+++ iknito_admin_panel/apps.py
@@ -3,11 +3,36 @@ iknito_admin_panel Django application initialization.
 """
 
 from django.apps import AppConfig
-
+from edx_django_utils.plugins.constants import (
+    ProjectType, SettingsType, PluginURLs, PluginSettings, PluginContexts
+)
 
 class IknitoAdminPanelConfig(AppConfig):
     """
     Configuration for the iknito_admin_panel Django application.
     """
 
-    name = 'iknito_admin_panel'
+    name = "iknito_admin_panel"
+    # Class attribute that configures and enables this app as a Plugin App.
+    plugin_app = {
+        # Configuration setting for Plugin URLs for this app.
+        PluginURLs.CONFIG: {
+            # Configure the Plugin URLs for each project type, as needed.
+            ProjectType.LMS: {
+                # The namespace to provide to django's urls.include.
+                PluginURLs.NAMESPACE: "iknito_admin_panel",
+                # The application namespace to provide to django's urls.include.
+                # Optional; Defaults to None.
+                PluginURLs.APP_NAME: "iknito_admin_panel",
+                # The regex to provide to django's urls.url.
+                # Optional; Defaults to r''.
+                PluginURLs.REGEX: r"^api/iknito_admin_panel/",
+                # The python path (relative to this app) to the URLs module to be plugged into the project.
+                # Optional; Defaults to 'urls'.
+                PluginURLs.RELATIVE_PATH: "urls",
+            }
+        },
+
+        
+
+    }
